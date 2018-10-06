2033d996fd59bc0f27522301e7d0ea1a602616f2
Dat Tran <dattran@success-ss.com.vn>
2018-10-06 18:05:41 +0700

    update url api service

--- src/app/services/apis.service.ts
+++ src/app/services/apis.service.ts
@@ -3,44 +3,44 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ApisService {
 
-  constructor() { }
-  baseUrl = "http://localhost:4201/";
+constructor() { }
+    baseUrl = "http://18.224.135.207:8000/";
 
-  authentication = { 
-      login: { method: 'POST', url: this.baseUrl + 'users' }
-  };
+    authentication = { 
+        login: { method: 'POST', url: this.baseUrl + 'users' }
+    };
 
-  donViLamViec = {
-      getAll: { method: 'GET',    url: this.baseUrl + 'donvilamviec' },
-      get   : { method: 'GET',    url: this.baseUrl + 'donvilamviec/' },
-      add   : { method: 'POST',   url: this.baseUrl + 'donvilamviec' },
-      update: { method: 'PUT',    url: this.baseUrl + 'donvilamviec' },
-      delete: { method: 'DELETE', url: this.baseUrl + 'donvilamviec/' }
-  };
+    donViLamViec = {
+        getAll: { method: 'GET',    url: this.baseUrl + 'donvilamviec' },
+        get   : { method: 'GET',    url: this.baseUrl + 'donvilamviec/' },
+        add   : { method: 'POST',   url: this.baseUrl + 'donvilamviec' },
+        update: { method: 'PUT',    url: this.baseUrl + 'donvilamviec' },
+        delete: { method: 'DELETE', url: this.baseUrl + 'donvilamviec/' }
+    };
 
-  danhMucPhuTung = {
-      getAll  : { method: 'GET',    url: this.baseUrl + 'danhmucphutung' },
-      get     : { method: 'GET',    url: this.baseUrl + 'danhmucphutung/' },
-      add     : { method: 'POST',   url: this.baseUrl + 'danhmucphutung' },
-      update  : { method: 'PUT',    url: this.baseUrl + 'danhmucphutung' },
-      delete  : { method: 'DELETE', url: this.baseUrl + 'danhmucphutung/' }
-  };
+    danhMucPhuTung = {
+        getAll  : { method: 'GET',    url: this.baseUrl + 'danhmucphutung' },
+        get     : { method: 'GET',    url: this.baseUrl + 'danhmucphutung/' },
+        add     : { method: 'POST',   url: this.baseUrl + 'danhmucphutung' },
+        update  : { method: 'PUT',    url: this.baseUrl + 'danhmucphutung' },
+        delete  : { method: 'DELETE', url: this.baseUrl + 'danhmucphutung/' }
+    };
 
-  hangXe={
-      getAll  : { method: 'GET',     url: this.baseUrl + 'hangxe' },
-      get     : { method: 'GET',     url: this.baseUrl + 'hangxe/' },
-      add     : { method: 'POST',    url: this.baseUrl + 'hangxe' },
-      update  : { method: 'PUT',     url: this.baseUrl + 'hangxe' },
-      delete  : { method: 'DELETE',  url: this.baseUrl + 'hangxe/' }
-  };
+    hangXe={
+        getAll  : { method: 'GET',     url: this.baseUrl + 'hangxe' },
+        get     : { method: 'GET',     url: this.baseUrl + 'hangxe/' },
+        add     : { method: 'POST',    url: this.baseUrl + 'hangxe' },
+        update  : { method: 'PUT',     url: this.baseUrl + 'hangxe' },
+        delete  : { method: 'DELETE',  url: this.baseUrl + 'hangxe/' }
+    };
 
-  dongXe={
-      getAll          :  { method: 'GET',     url: this.baseUrl + 'dongxe' },
-      get             :  { method: 'GET',     url: this.baseUrl + 'dongxe/' },
-      add             :  { method: 'POST',    url: this.baseUrl + 'dongxe' },
-      update          :  { method: 'PUT',     url: this.baseUrl + 'dongxe' },
-      delete          :  { method: 'DELETE',  url: this.baseUrl + 'dongxe/' },
-      getDongXeHangXe :  { method: 'GET',     url: this.baseUrl + 'dongxe/mahangxe/' }
-  };
+    dongXe={
+        getAll          :  { method: 'GET',     url: this.baseUrl + 'dongxe' },
+        get             :  { method: 'GET',     url: this.baseUrl + 'dongxe/' },
+        add             :  { method: 'POST',    url: this.baseUrl + 'dongxe' },
+        update          :  { method: 'PUT',     url: this.baseUrl + 'dongxe' },
+        delete          :  { method: 'DELETE',  url: this.baseUrl + 'dongxe/' },
+        getDongXeHangXe :  { method: 'GET',     url: this.baseUrl + 'dongxe/mahangxe/' }
+    };
 
 }
